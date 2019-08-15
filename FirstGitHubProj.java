3fe9af62465c9dc82ef5e2b129d0cdca1f9d1609
571327732 <571327732@qq.com>
2019-08-15 19:43:15 +0800

    update firstGitHubProj.java

--- FirstGitHubProj.java
+++ FirstGitHubProj.java
@@ -3,5 +3,7 @@ public class FirstGitHubProj {
 
 	public static void main(String[] args) {
 		System.out.println("hello,FirstGitHubProj!!!!!");
+                                System.out.println("FirstGitHubProj.java");
+System.out.println("FirstGitHubProj.java");
 	}
 }
